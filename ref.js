99c7a13fda6f3573147578174183c8dce0452d7e
Jiahe Jou <joujiahe@gmail.com>
2013-03-11 18:36:48 +0800

    Basic functions completed.
    Signed-off-by: Jiahe Jou <joujiahe@gmail.com>

--- ref.js
+++ ref.js
@@ -1,13 +1,62 @@
+Posts = new Meteor.Collection('posts');
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to ref.";
+  // Posts
+  Template.posts.posts = function(){
+    return Posts.find({}, {sort: {createdAt: -1}});
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  // Return an event map for a text input.
+  var entry_event = function(selector) {
+    return 'keyup '+selector+', keydown '+selector+', focusout '+selector;
+  };
+
+  var entry_event_handler = function(options) {
+    var ok = options.ok || function() {};
+    var cancel = options.ok || function() {};
+
+    return function(evt) {
+      if (evt.type === 'keydown' && evt.which == 27) {
+        // escape is cancel
+        cancel.call(this, evt);
+      } else if (evt.type === 'keyup' && evt.which == 13 ) {
+        // blur/return/enter is ok if not empty
+        var value = String(evt.target.value || '');
+        if (value)
+          ok.call(this, value, evt);
+        else
+          cancel.call(this, evt);
+      }
+    };
+  };
+
+  Template.entry.events = {};
+  Template.entry.events[entry_event('#content')] = entry_event_handler({
+    ok: function(text, evt) {
+      var titleEntry = document.getElementById('title');
+      var time = Date.now() / 1000;
+      Posts.insert({userId: Meteor.userId(), title: titleEntry.value, content: text, createdAt: time});
+      titleEntry.value = '';
+      evt.target.value = '';
+    }
+  });
+
+  Template.post.createdDatetime = function() {
+    var dt = new Date(this.createdAt*1000);
+    return dt.toLocaleString();
+    //return dt.toDateString()+' '+dt.getHours()+':'+dt.getMinutes()+':'+dt.getSeconds();
+  }
+
+  Template.post.getScore = function(score) {
+    return this.score? this.score: 0;
+  }
+
+  Template.post.events({
+    'click .up': function(){
+      Posts.update(this._id, {$inc: {score: 1}});
+    },
+    'click .down': function(){
+      Posts.update(this._id, {$inc: {score: -1}});
     }
   });
 }
